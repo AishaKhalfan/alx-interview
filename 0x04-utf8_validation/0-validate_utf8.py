be68b802dc81d23a494811be2207300bf9a3f698
AishaKhalfan <aishakhalfan45@gmail.com>
2023-08-10 21:04:32 +0300

    utf-8 validation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 """
-
+UTF-8 Validation
 """
-def def validUTF8(data)
+
+
+def validUTF8(data):
     """
     determines if a given data set represents a valid UTF-8 encoding.
     Args:
@@ -11,4 +13,13 @@ def def validUTF8(data)
         True - if data is a valid UTF-8 encoding,
         False -  if data is not a valid UTF-8 encoding,
     """
-    
+    successive_10 = 0
+    for b in data:
+        b = bin(b).replace('0b', '').rjust(8, '0')
+        if successive_10 != 0:
+            successive_10 -= 1
+            if not b.startswith('10'):
+                return False
+        elif b[0] == '1':
+                successive_10 = len(b.split('0')[0]) - 1
+    return True
