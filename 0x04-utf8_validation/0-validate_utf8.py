7e9504646d7f20648d14d1f7724d18e6ecda4bc0
AishaKhalfan <aishakhalfan45@gmail.com>
2023-08-10 22:19:20 +0300

    utf8_validation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,25 +1,40 @@
 #!/usr/bin/python3
-"""
-UTF-8 Validation
-"""
+"""Determines a valid UTF-8 encoding"""
 
 
 def validUTF8(data):
     """
-    determines if a given data set represents a valid UTF-8 encoding.
-    Args:
-        data
-    Return:
-        True - if data is a valid UTF-8 encoding,
-        False -  if data is not a valid UTF-8 encoding,
+    bit1 checks if significant byte is 1
+    bit2 checks if second significant byte is 0
+    nbytes keeps track of how many 1s before 0 occurs
+    data represented by a list of integers to check
     """
-    successive_10 = 0
-    for b in data:
-        b = bin(b).replace('0b', '').rjust(8, '0')
-        if successive_10 != 0:
-            successive_10 -= 1
-            if not b.startswith('10'):
+
+    bit1 = 1 << 7
+    bit2 = 1 << 6
+    nbytes = 0
+
+    if not data or len(data) == 0:
+        return True
+
+    for num in data:
+        bit = 1 << 7
+        if nbytes == 0:
+            while (bit & num):
+                nbytes += 1
+                bit = bit >> 1
+
+            if nbytes == 0:
+                continue
+            if nbytes == 1 or nbytes > 4:
                 return False
-        elif b[0] == '1':
-                successive_10 = len(b.split('0')[0]) - 1
-    return True
+        else:
+
+            if not (num & bit1 and not (num & bit2)):
+                return False
+        nbytes -= 1
+
+    if nbytes:
+        return False
+    else:
+        return True
