d702ec9d056fafabd3d41935149ae22297c761fb
AishaKhalfan <aishakhalfan45@gmail.com>
2023-08-17 16:39:23 +0300

    2d matrix rotation

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,10 +1,14 @@
+#!/usr/bin/python3
 import numpy as np
 
 def rotate_2d_matrix(matrix):
-  rotated = np.array(matrix)
+    """
+    an n x n 2D matrix, rotate it 90 degrees clockwise.
+    """
+    rotated = np.array(matrix)
 
-  # In-place swap of rows and columns
-  np.rot90(rotated, k=1, axes=(0,1))
-  
-  # Override original matrix
-  matrix[:] = rotated
+    # In-place swap of rows and columns
+    np.rot90(rotated, k=1, axes=(0,1))
+
+    # Override original matrix
+    matrix[:] = rotated
