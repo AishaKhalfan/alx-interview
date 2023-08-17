dfbf2069e3b6b6638431120359b4fe7e2b09bb5a
AishaKhalfan <aishakhalfan45@gmail.com>
2023-08-17 20:55:35 +0300

    2D matrix, rotate it 90 degrees clockwise

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,14 +1,19 @@
 #!/usr/bin/python3
-import numpy as np
+"""
+Test 0x07 - Rotate 2D Matrix
+"""
 
 def rotate_2d_matrix(matrix):
     """
-    an n x n 2D matrix, rotate it 90 degrees clockwise.
+    n x n 2D matrix, rotate it 90 degrees clockwise.
     """
-    rotated = np.array(matrix)
+    n = len(matrix)
 
-    # In-place swap of rows and columns
-    np.rot90(rotated, k=1, axes=(0,1))
+    # Transpose the matrix 
+    for i in range(n):
+        for j in range(i, n):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
 
-    # Override original matrix
-    matrix[:] = rotated
+    # Reverse rows
+    for i in range(n):
+        matrix[i].reverse()
--- 0x07-rotate_2d_matrix/notinplace.py
+++ 0x07-rotate_2d_matrix/notinplace.py
@@ -19,4 +19,11 @@ def rotate_2d_matrix(matrix):
     # Reverse rows
     for i in range(n):
         matrix[i].reverse()
-    
+
+matrix = [[1, 2, 3],
+          [4, 5, 6],
+          [7, 8, 9]]
+
+rotate_2d_matrix(matrix)
+print(matrix)
+
